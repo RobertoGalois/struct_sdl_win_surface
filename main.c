c5802c1a50e34bd5460fc1cfdf67542ab9b4ce4c
RobertoGalois <victor.vague@live.fr>
2017-08-09 20:43:03 +0200

    Update main.c

--- main.c
+++ main.c
@@ -28,13 +28,13 @@ void    check_SDL_Init(SDL_Surface* p_element)
 /* transform 3 unsigned short into an unsigned short[]) */
 unsigned short*     int_to_color(unsigned short p_r, unsigned short p_g, unsigned short p_b)
 {
-    unsigned short* ret_tab;
+                    unsigned short* ret_tab;
 
-    ret_tab = malloc(3 * sizeof(unsigned short));
-    *(ret_tab) = p_r;
-    *(ret_tab+1) = p_g;
-    *(ret_tab+2) = p_b;
-    return (ret_tab);
+                    ret_tab = malloc(3 * sizeof(unsigned short));
+                    *(ret_tab) = p_r;
+                    *(ret_tab+1) = p_g;
+                    *(ret_tab+2) = p_b;
+                    return (ret_tab);
 }
 
 struct  s_window*   init_s_window()
