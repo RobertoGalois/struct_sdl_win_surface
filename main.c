18931964a44f819d898726eeb52bbf3b42f5ccf0
RobertoGalois <victor.vague@live.fr>
2017-08-10 19:38:28 +0200

    Update main.c

--- main.c
+++ main.c
@@ -52,6 +52,32 @@ struct  s_window*   init_s_window(unsigned int p_w, unsigned int p_h, char* p_ti
         return (ret_window);
 }
 
+struct  s_surface* init_s_surface(SDL_Surface* p_window, unsigned int p_w, unsigned int p_h, unsigned int p_x, unsigned int p_y, unsigned short p_color[])
+{
+        struct s_surface*   ret_s_surface;  /* ptr ret sur la structure init */
+
+        /* creation de la structure */
+        ret_s_surface = malloc(sizeof(struct s_surface));
+
+        /* initialisation de la surface sdl de la struct */
+        ret_s_surface->sdl_surf = SDL_CreateRGBSurface(SDL_HWSURFACE, p_w, p_h,
+                                                        32, 0, 0, 0, 0); 
+
+        /* coloration de la surface sdl */
+        SDL_FillRect(ret_s_surface->sdl_surf, 
+                        NULL, SDL_MapRGB(p_window->format,
+                        p_color[0], p_color[1], p_color[2]));
+
+        /* definition des coordonnees */
+        (*ret_s_surface).coord.x = p_x;
+        (*ret_s_surface).coord.y = p_y;
+
+        /* append */
+        SDL_BlitSurface(ret_s_surface->sdl_surf,
+                        NULL, p_window, &(ret_s_surface->coord));
+
+        return (ret_s_surface);
+}
 
 void    ev_loop(void)
 {
