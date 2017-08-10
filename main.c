788ccf603817cb767187d40648fd7991087b6975
RobertoGalois <victor.vague@live.fr>
2017-08-10 19:12:50 +0200

    Update main.c

--- main.c
+++ main.c
@@ -37,19 +37,21 @@ unsigned short*     int_to_color(unsigned short p_r, unsigned short p_g, unsigne
                     return (ret_tab);
 }
 
-struct  s_window*   init_s_window()
+struct  s_window*   init_s_window(unsigned int p_w, unsigned int p_h, char* p_title, unsigned short color[])
 {
         struct s_window* ret_window;
 
         ret_window = malloc(sizeof(struct s_window));
-        ret_window->sdl_win = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+        ret_window->sdl_win = SDL_SetVideoMode(p_w, p_h, 32, SDL_HWSURFACE);
         check_SDL_Init(ret_window->sdl_win);
-        SDL_WM_SetCaption("Ma super fenêtre SDL !", NULL);
+        SDL_WM_SetCaption(p_title, NULL);
         SDL_FillRect(ret_window->sdl_win, NULL, 
-                    SDL_MapRGB((ret_window->sdl_win)->format, 255, 255, 255));
+                    SDL_MapRGB((ret_window->sdl_win)->format,
+                                color[0], color[1], color[2]));
 
         return (ret_window);
 }
+
 
 void    ev_loop(void)
 {
